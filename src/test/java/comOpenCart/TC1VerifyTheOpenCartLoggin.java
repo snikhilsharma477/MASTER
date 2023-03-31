7e92ec8af76d0741b6c716dc2ffe5eb6198fea3f
snikhilsharma477 <snikhilsharma477@gmail.com>
2023-03-31 19:40:21 +0530

    update

--- src/test/java/comOpenCart/TC1VerifyTheOpenCartLoggin.java
+++ src/test/java/comOpenCart/TC1VerifyTheOpenCartLoggin.java
@@ -18,11 +18,8 @@ import static TestUtility.TestUtilities.readingExcelFile;
 
 
 public class TC1VerifyTheOpenCartLoggin  extends TestBase {
-
-
     @Test
 public void ValidateLoging() throws IOException {
-// login
 
         initialize();
 
